d1aac8191322ff7a495a3e014a91b072314b77a0
DP DEV <duilioperez1205@gmail.com>
2024-05-18 16:57:47 -0600

    Bug fixed that hangs apps

--- include/DPGE/Label.hpp
+++ include/DPGE/Label.hpp
@@ -3,8 +3,10 @@
 /// @brief The label widget.
 #ifndef LABEL_HPP
 #define LABEL_HPP
+#include "TextureManager.hpp"
 #include "Widget.hpp"
 #include <SDL2/SDL.h>
+#include <list>
 
 namespace DPGE
 {
@@ -13,16 +15,34 @@ namespace DPGE
   class Label final : public Widget
   {
   public:
-    // Overriden functions.
-    void handleEvents() override;
-    void update() override;
+    /// @brief Default constructor.
+    Label() = default;
+    /// @brief Constructor
+    /// @param labelArea The area of the label.
+    explicit Label(const SDL_Rect &labelArea);
+    /// @brief Set the area of the label.
+    /// @param labelArea The area of the label.
+    void setArea(const SDL_Rect &labelArea);
+    /// @brief Get the area of the label.
+    /// @return The area of the label.
+    const SDL_Rect &getArea() const;
+    /// @brief Get the layers of the label.
+    /// @return The layers of the label.
+    const std::list<TextureInfo> &getLayers() const;
+    /// @brief Set the layers of the label.
+    /// @param layers The layers of the label.
+    void setLayers(const std::list<TextureInfo> &layers);
+    /// @brief Update the current widget
+    /// @param function The function to call to update.
+    void update(void (*function)(std::list<TextureInfo> &));
+    /// @brief Overriden funtion to render the widget.
     void render() override;
 
   private:
+    /// @brief The information of the textures of the label.
+    std::list<TextureInfo> layers;
     /// @brief The area of the label.
     SDL_Rect area;
-    /// @brief The label's string.
-    const char *text = nullptr;
   };
 
 } // namespace DPGE
